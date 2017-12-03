a214b35b990aa998a1fce8a52b1d0ba44d9a77f5
firecold100 <c_m_a1990@live.com>
2017-12-02 22:11:11 -0600

    Esbozo de la paǵina del chat hecho. Falta AJAX y la conexión con la BD Disponible al correro el server: http://localhost:8085/gustosculposos/chat

---
+++ gustosculposos/src/main/webapp/js/chat.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @file chat.js
+ * @author Alan Moreno de la Rosa
+ * @version 1.0 07/12/2017
+ */
+
+/**
+ *  Función que carga cuando el DOM se encuentra completamente cargado y listo
+ */
+$(document).ready(function(){
+    
+});
+
+
